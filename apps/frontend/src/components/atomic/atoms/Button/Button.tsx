eeaf090844f61098b82bfa479968fe36d7c64620
Nguyen An <nguyenan21062005ne@gmail.com>
2026-07-10 04:32:06 +0700

    fix: bug size img .svg

--- apps/frontend/src/components/atomic/atoms/Button/Button.tsx
+++ apps/frontend/src/components/atomic/atoms/Button/Button.tsx
@@ -3,36 +3,36 @@ import { Icon } from "@/components/atomic/atoms"
 export interface ButtonIconProps {
     iconPosition: "left" | "right",
     iconName: string,
-    size?: number
+    size?: keyof typeof SIZE_CLASSES
 }
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     text?: string,
-    fgColor?: string,
-    bgColor?: string
-    hoverbgColor?: string,
-    hoverfgColor?: string,
     className?: string,
     buttonIconProps?: ButtonIconProps,
 }
+
+const SIZE_CLASSES = {
+  small: 'w-5 h-5',
+  medium: 'w-6 h-6',
+  large: 'w-7 h-7',
+};
 
 export const Button = (
     {
         type = 'button',
         text = 'default',
         buttonIconProps,
-        fgColor = 'text-main',
-        bgColor = '',
         className = ''
     }: ButtonProps
 ) => {
-    const iconSize = buttonIconProps?.size ?? 5
+    const iconSize = buttonIconProps?.size ?? "small";
     const directionClass = buttonIconProps?.iconPosition === 'left' ? 'flex-row-reverse' : 'flex-row';
     return (
-        <button type={type} className={`cursor-pointer flex items-center ${directionClass} bg-${bgColor} fg-${fgColor} ${className}`}>
+        <button type={type} className={`cursor-pointer flex items-center ${directionClass} ${className}`}>
             <div>{text}</div>
             {buttonIconProps &&
-                <div className={`size-${iconSize}`}>
+                <div className={`${SIZE_CLASSES[iconSize]}`}>
                     <Icon name={buttonIconProps.iconName} />
                 </div>
             }
